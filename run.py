d23914224ad51a63f386af4dd1f48bbbc20a49e7
Paul Sandoval Quilodrán <96622570+SQPaul@users.noreply.github.com>
2022-11-22 16:04:08 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 13 15:57:14 2022
+
+@author: pauls
+"""
+
+import os
+
+os.system("python -m SPHY.main -i c:\juncal_norte\sphy_config_juncal_norte.cfg")
